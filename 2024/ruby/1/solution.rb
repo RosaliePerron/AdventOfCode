d65c894da54edb0dfa029ae458671f789810fd1c
Rosalie Perron <gritty.joy9372@fastmail.com>
2025-11-13 17:50:06 -0500

    2024 ruby | day 1, part 2

--- 2024/ruby/1/solution.rb
+++ 2024/ruby/1/solution.rb
@@ -13,9 +13,36 @@ end
 first = first.sort 
 second = second.sort
 
-total = 0
-first.each_with_index do |nb, i|
-  total += (nb - second[i]).abs
+def part_one(first, second)
+  total = 0
+  first.each_with_index do |nb, i|
+    total += (nb - second[i]).abs
+  end
+
+  total
 end
 
-puts total
+def part_two(first, second)
+  total = 0
+  first.each do |nb|
+    total += nb * second.select { |n| n == nb }.length
+  end
+
+  total
+end
+
+def part_two_alt(first, second)
+  map = {}
+  second.each do |nb|
+    map[nb] = (map[nb] || 0) + 1
+  end
+
+  total = 0
+  first.each do |nb|
+    total += nb * (map[nb] || 0)
+  end
+  
+  total
+end
+
+puts part_two_alt(first, second)
