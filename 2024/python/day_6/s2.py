ea253f88dc5647cdc5c11d48723d0469c97067e3
Rosalie Perron <gritty.joy9372@fastmail.com>
2025-11-13 07:38:58 -0500

    move into 2024 and python folders

---
+++ 2024/python/day_6/s2.py
@@ -0,0 +1,124 @@
+INPUT_FILE = "input_t"
+
+
+def read_input():
+    with open(INPUT_FILE, "r") as f:
+        current_position = [0, 0, "^"]
+        obstacles = []
+        room_dimensions = [0, 0]
+
+        y = 1
+        for line in f:
+            if room_dimensions[0] == 0:
+                room_dimensions[0] = len(line) - 1
+
+            x = 1
+            for position in line:
+                if position == "#":
+                    obstacles.append((x, y))
+
+                if position in ("^", "<", ">", "v"):
+                    current_position = [x, y, position]
+
+                x += 1
+            y += 1
+
+        room_dimensions[1] = y - 1
+        return current_position, obstacles, room_dimensions
+
+
+def travel_to_next_obstacle(current_position, obstacles, room_dimensions):
+    print(current_position)
+    x, y, direction = current_position
+    positions = []
+
+    coords = [current_position[0], current_position[1]]
+    i = 1
+    while coords not in obstacles and room_dimensions[0] >= coords[0] > 0 and room_dimensions[1] >= coords[1] > 0:
+        footsteps = ()
+        if direction == "^":
+            coords = [x, y - i]
+            footsteps = (x, y - i, "|")
+
+        elif direction == ">":
+            coords = [x + i, y]
+            footsteps = (x + i, y, "-")
+
+        elif direction == "v":
+            coords = [x, y + i]
+            footsteps = (x, y + i, "|")
+
+        elif direction == "<":
+            coords = [x - i, y]
+            footsteps = (x - i, y, "-")
+
+        positions.append(footsteps)
+        i += 1
+
+    return positions
+
+
+def get_next_direction(current_direction):
+    if current_direction == "^":
+        return ">"
+    elif current_direction == ">":
+        return "v"
+    elif current_direction == "v":
+        return "<"
+    elif current_direction == "<":
+        return "^"
+
+
+def compute_path(data):
+    current_position, obstacles, room_dimensions = data
+    footsteps = [current_position]
+
+    exited_the_room = False
+    while not exited_the_room:
+        footsteps += travel_to_next_obstacle(
+            current_position, 
+            obstacles, 
+            room_dimensions
+        )
+        last_direction = current_position[2]
+        last_position_of_path = footsteps[-2]
+        current_position = [last_position_of_path[0], last_position_of_path[1], get_next_direction(last_direction)]
+
+        exited_the_room = (
+            current_position[0] == 0 
+            or current_position[1] == 0
+            or current_position[0] == room_dimensions[0]
+            or current_position[1] == room_dimensions[1]
+        )
+
+    print_room(room_dimensions, obstacles, footsteps)
+
+
+def print_room(room_dimensions, obstacles, positions):
+    result = 0
+    print(positions)
+    position_coords = [[pos[0], pos[1]] for pos in positions]
+    y = 1
+    for line in range(room_dimensions[1]):
+
+        x = 1
+        for position in range(room_dimensions[0]):
+            if (x, y) in obstacles:
+                print("#", end="")
+
+            elif (x, y) in position_coords:
+                print("x", end="")
+                result += 1
+
+            else:
+                print(".", end="")
+            x += 1
+
+        print()
+        y += 1
+
+    print(f"{result} total positions visited.")
+
+
+if __name__ == "__main__":
+    compute_path(read_input())
